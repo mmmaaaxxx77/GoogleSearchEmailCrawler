0df493be408094c1f8b1560ef4c2e323ed06975e
johnny <mmmaaaxxx77@gmail.com>
2018-10-16 14:56:20 +0800

    no message

--- api/shop/settings.py
+++ api/shop/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join("/db", 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
